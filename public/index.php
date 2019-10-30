464508b8cef8a23842bd2717df192dac571774c6
hojinlee <infohojin@gmail.com>
2019-10-30 12:22:36 +0900

    테이블 함수 변환

--- public/index.php
+++ public/index.php
@@ -16,14 +16,44 @@ $result = $db->queryExecute($query);
 
 $count = mysqli_num_rows($result);
 $content = ""; // 초기화
+$rows = []; // 배열 초기화
 for ($i=0;$i<$count;$i++) {
     $row = mysqli_fetch_object($result);
+    $rows []= $row; // 배열 추가
+    /*
     $content .= "<tr>";
     $content .= "<td>$i</td>";
     $content .= "<td>".$row->Tables_in_php."</td>";
     $content .= "</tr>";
+    */
 }
+$content = table($rows);
 
 $body = file_get_contents("../Resource/table.html");
 $body = str_replace("{{content}}",$content, $body); // 데이터 치환
 echo $body;
+
+function table($rows)
+{
+    $body = "<table class=\"table\">";
+    $body .= "<thead>";
+    $body .= "<tr>
+        <th>번호</th>
+        <th>테이블명</th>
+      </tr>";
+    $body .= "</thead>";
+    $body .= "<tbody>";
+    
+    // https://github.com/infohojin/php_www
+
+    for($i=0;$i<count($rows);$i++) {
+        $body .= "<tr>";
+        $body .= "<td>$i</td>";
+        $body .= "<td>".$rows[$i]->Tables_in_php."</td>";
+        $body .= "</tr>";
+    }    
+
+    $body .= "</tbody>";
+    $body .= "</table>";
+    return $body;
+}
