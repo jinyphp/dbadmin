e3d8383ba96239b32cc624bbfb5a6b2ccdc3150b
kimminha371 <kkk_92112@naver.com>
2019-11-06 15:09:18 +0900

    dbconf 추가

---
+++ dbconf.php
@@ -0,0 +1,14 @@
+<?php
+//서버정보
+$host = "localhost";
+$user = "phpuser";
+$passwd = "multi1004";
+$database = "php";
+
+// 여러개를 반환하고싶으면 배열이나 객체 사용
+return [
+    "host"=>$host, 
+    "user"=>$user, 
+    "passwd"=>$passwd, 
+    "database"=>$database
+];
