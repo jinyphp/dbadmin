f60ae38ce4b5a86f8119c5af9c527a148e540677
Hminji <hminji4565@gmail.com>
2019-11-06 14:58:38 +0900

    loding.php파일 추가

---
+++ Loading.php
@@ -0,0 +1,9 @@
+<?php
+// 자동으로 require 해주는 기능
+// 이 기능을 등록해 준다. // 익명 함수로 전달
+spl_autoload_register(function($classname){
+    //echo $classname;
+    // 클래스 이름을 조합해서, require 해준다.
+    require "../".$classname.".php";
+    //exit;
+});
