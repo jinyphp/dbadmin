92d65ec818e179564058c1ac8c15af91b6118bd7
HmLee1024 <rhrlfkd1111@naver.com>
2019-11-06 15:18:28 +0900

    uri.php파일추가

---
+++ Module/Http/Uri.php
@@ -0,0 +1,44 @@
+<?php
+namespace Module\Http;
+
+// 선언 -> 생성 -> 호출
+// 데이터베이스 선언
+class Uri
+{
+    public $uri;    //외부 접근 허용
+    private $uris;
+    
+    public function __construct()
+    {
+        //echo __CLASS__;
+        $this->uri = $_SERVER['REQUEST_URI'];
+        $this->uris = explode("/",$this->uri);
+        unset($this->uris[0]);  // 0번 배열 제거
+    }
+
+    public function first()
+    {
+        if(isset($this->uris[1]) && $this->uris[1])
+        {
+            return $this->uris[1];
+        }
+    }
+
+    public function second()
+    {
+        if(isset($this->uris[2]) && $this->uris[2])
+        {
+            return $this->uris[2];
+        }
+    }
+
+    public function third()
+    {
+        if(isset($this->uris[3]) && $this->uris[3])
+        {
+            return $this->uris[3];
+        }
+    }
+
+
+}
