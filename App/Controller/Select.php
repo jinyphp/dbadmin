86ca5562b9953c33017547a0113e957f526884a8
hojinlee <infohojin@gmail.com>
2019-11-06 12:51:13 +0900

    data insert

--- App/Controller/Select.php
+++ App/Controller/Select.php
@@ -3,23 +3,71 @@ namespace App\Controller;
 class Select
 {
     private $db;
+    private $HttpUri;
+
+    private $Html;
+
     // 생성자
     public function __construct($db)
     {
         // echo __CLASS__;
         $this->db = $db;
+        $this->HttpUri = new \Module\Http\Uri();
+        $this->Html = new \Module\Html\HtmlTable;
     }
 
+    /**
+     * 처음 동작 구분 처리
+     */
     public function main()
+    {        
+        $tableName = $this->HttpUri->second();
+        if ($this->HttpUri->third() == "new") {
+            echo "새로운 데이터 입력";
+            $this->newInsert($tableName);
+        } else {
+            $this->list($tableName);
+        }        
+    }
+
+    /**
+     * 새로운 데이터를 입력
+     */
+    public function newInsert($tableName)
     {
-        $html = new \Module\Html\HtmlTable;
 
-        $uri = $_SERVER['REQUEST_URI'];
-        $uris = explode("/",$uri); // 파란책
-        // []/[select]/[members]
+        print_r($_POST);
+        if ($_POST) {
+            $query = "INSERT INTO ".$tableName." (`FirstName`,`LastName`) 
+                        VALUES ('".$_POST['firstname']."','".$_POST['lastname']."')";
+            $result = $this->db->queryExecute($query);
+            
+            // 페이지 이동
+            header("location:"."/select/".$tableName);
+        }
 
-        if ( isset($uris[2]) && $uris[2] ) {
-            $query = "SELECT * from ".$uris[2]; // SQL 쿼리문
+        // echo "메인 호출이에요.";
+        $query = "DESC ".$tableName;
+        $result = $this->db->queryExecute($query);
+
+        $count = mysqli_num_rows($result);
+        $content = ""; // 초기화
+        $rows = []; // 배열 초기화
+        for ($i=0;$i<$count;$i++) {
+            $row = mysqli_fetch_object($result);
+            $rows []= $row; // 배열 추가 (2차원 배열)
+        }
+        $content = $this->Html->table($rows);
+
+        $body = file_get_contents("../Resource/insert.html");
+        $body = str_replace("{{content}}",$content, $body); // 데이터 치환
+        echo $body;
+    }
+
+    public function list($tableName)
+    {        
+        if ( $tableName ) {
+            $query = "SELECT * from ".$tableName; // SQL 쿼리문
             $result = $this->db->queryExecute($query);
 
             $content = ""; // 초기화
@@ -34,7 +82,7 @@ class Select
                     $rows []= $row; // 배열 추가 (2차원 배열)
                 }
         
-                $content = $html->table($rows);
+                $content = $this->Html->table($rows);
             } else {
                 // 데이터가 없음.
                 $content = "데이터 없음";
@@ -42,9 +90,6 @@ class Select
         } else {
             $content = "선택된 테이블이 없습니다.";
         }
-
-        
-        
 
         $body = file_get_contents("../Resource/select.html");
         $body = str_replace("{{content}}",$content, $body); // 데이터 치환
---
+++ Module/Http/Uri.php
@@ -0,0 +1,40 @@
+<?php
+namespace Module\Http;
+
+// 선언 -> 생성 -> 호출
+// 데이터베이스 선언
+class Uri
+{
+    public $uri; // 외부접근 허용
+    private $uris; // 내부만
+
+    public function __construct()
+    {
+        echo __CLASS__;
+        $this->uri= $_SERVER['REQUEST_URI'];
+        $this->uris = explode("/",$this->uri); // 파란책
+        unset($this->uris[0]); // 0번 배열은 제거
+    }
+
+    public function first()
+    {
+        if(isset($this->uris[1]) && $this->uris[1]) {
+            return $this->uris[1];
+        }
+    }
+
+    public function second()
+    {
+        if(isset($this->uris[2]) && $this->uris[2]) {
+            return $this->uris[2];
+        }
+    }
+
+    public function third()
+    {
+        if(isset($this->uris[3]) && $this->uris[3]) {
+            return $this->uris[3];
+        }
+    }
+
+}
